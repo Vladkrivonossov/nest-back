67a6c1fd96725e4b2c48051abcdc39211eb863b4
Vladkrivonossov <vladik374374@gmail.com>
2023-01-18 13:46:13 +0300

    fix authentification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -51,13 +51,15 @@ export class AuthService {
 
   private async validateUser(userDto: CreateUserDto) {
     const user = await this.userService.getUserByEmail(userDto.email);
-    const passwordEquals = await bcrypt.compare(
-      userDto.password,
-      user.password,
-    );
-
-    if (user && passwordEquals) {
-      return user;
+    if (user) {
+      const passwordEquals = await bcrypt.compare(
+        userDto.password,
+        user.password,
+      );
+  
+      if (passwordEquals) {
+        return user;
+      }
     }
 
     throw new UnauthorizedException({ message: 'Wrong email or password' });
